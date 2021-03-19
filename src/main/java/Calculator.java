1ce4a4e2d95fea89ea393de3db47e23753c43ec1
yqx1314 <2269941675@qq.com>
2021-03-19 19:48:02 +0800

    refactor calculator to use reflect

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,7 +6,7 @@
  */
 public class Calculator {
 
-    public int compute(int num1, int num2, char symbol) {
+    public int compute(int num1, int num2, String symbol) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
         Computable computable = ComputeFactory.getCompute(symbol);
         return computable.compute(num1,num2);
     }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -9,17 +9,17 @@ import org.junit.Test;
  */
 public class CalculatorTest {
     @Test
-    public void shouldReturn2When1Plus1(){
+    public void shouldReturn2When1Plus1() throws IllegalAccessException, ClassNotFoundException, InstantiationException {
         Calculator calculator = new Calculator();
-        int actualResult = calculator.compute(1,1,'+');
+        int actualResult = calculator.compute(1,1,"Add");
         int expectResult = 2;
         Assert.assertEquals(expectResult,actualResult);
     }
 
     @Test
-    public void shouldReturn0When1Sub1(){
+    public void shouldReturn0When1Sub1() throws IllegalAccessException, ClassNotFoundException, InstantiationException {
         Calculator calculator = new Calculator();
-        int actualResult = calculator.compute(1,1,'-');
+        int actualResult = calculator.compute(1,1,"Sub");
         int expectResult = 0;
         Assert.assertEquals(expectResult,actualResult);
     }
