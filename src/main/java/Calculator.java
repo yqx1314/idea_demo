269417ea232939c56738e2d61b96e92f05083dd9
yqx1314 <2269941675@qq.com>
2021-03-19 19:22:16 +0800

    add calculator code

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,6 +7,13 @@
 public class Calculator {
 
     public int compute(int num1, int num2, char symbol) {
-        return -1;
+        switch(symbol){
+            case '+':
+                return num1+num2;
+            case '-':
+                return num1-num2;
+            default:
+                throw new IllegalArgumentException();
+        }
     }
 }
